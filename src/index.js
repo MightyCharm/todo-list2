0eaefbf56139c86ea09173142f04f2a3c9355fbd
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-10-25 14:23:03 +0200

    feat(project-list): implement confirm and cancel functionality for project creation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import "./styles.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-
-
 // const btnAddProject = document.getElementById("btn-add-project");
 const projectList = document.getElementById("project-list");
 
 // btnAddProject.addEventListener("click", () => {
 //   console.log("create project");
 // })
-
-
 
 // note for later: allow only the creation of one instance of this class
 class ProjectManager {
@@ -25,7 +21,8 @@ class ProjectManager {
     const project = new Project(name);
     this.activeProject = project;
     this.projects.push(project);
-    console.log(`Project "${name} was added.`);
+    console.log(`Project "${name}" was added.`);
+    return project;
   }
 
   removeProject(id) {
@@ -155,75 +152,39 @@ class ToDo {
   }
 }
 
-
-/*
-const projectManager = new ProjectManager();
-projectManager.addProject("Chores");
-projectManager.addProject("Homework");
-projectManager.addProject("Friends");
-
-console.log(projectManager.getProjects());
-console.log(projectManager.getActiveProject());
-
-const projectDefault = projectManager.getProjects()[0];
-const projectHomework = projectManager.getProjects()[1];
-const projectFriends = projectManager.getProjects()[2];
-console.log(projectDefault);
-console.log(projectHomework);
-console.log(projectFriends)
-*/
-/*
-const idTodo_1 = projectDefault.addToDo(
-  "Meet with Kevin",
-  "Kevin called, its urgent, we meet in the city at cafe Vertigo",
-  "1.5.25",
-  "normal"
-);
-
-const idTodo_2 = projectDefault.addToDo(
-  "Call Grandpa",
-  "Grandpa asked about the books i ordered, Should call him back ASAP.",
-  "3.6.25",
-  "high"
-);
-
-const idTodo_3 = projectDefault.addToDo(
-  "Birthday Susanne",
-  "Still need to find a present.",
-  "22.6.25",
-  "normal"
-);
-*/
-
+// create Button Add +
 function createAddProject() {
   const listProjectName = document.getElementById("li-project-name");
-  if(listProjectName) {
+  if (listProjectName) {
     listProjectName.remove();
   }
-  
+
   const list = document.createElement("li");
   const button = document.createElement("button");
   const icon = document.createElement("i");
 
   list.id = "li-add-project";
-  button.id = "btn-add-project"
+  button.id = "btn-add-project";
   button.classList.add("btn-add-project");
   button.textContent = "NEW";
   icon.classList.add("fas", "fa-plus-circle");
 
-  button.appendChild(icon)
-  list.appendChild(button)
+  button.appendChild(icon);
+  list.appendChild(button);
   projectList.appendChild(list);
 
   button.addEventListener("click", () => {
     console.log("click");
     createInputProject();
-  })
+  });
 }
 
+// creates input and buttons to confirm/cancel 
 function createInputProject() {
   const listAddProject = document.getElementById("li-add-project");
-  listAddProject.remove();
+  if (listAddProject) {
+    listAddProject.remove();
+  }
 
   const list = document.createElement("li");
   const input = document.createElement("input");
@@ -232,7 +193,7 @@ function createInputProject() {
   const iconConfirm = document.createElement("i");
   const iconCancel = document.createElement("i");
 
-  list.id = "li-project-name"
+  list.id = "li-project-name";
   input.id = "input-project-name";
   input.type = "text";
   input.maxLength = "50";
@@ -254,18 +215,59 @@ function createInputProject() {
 
   btnConfirm.addEventListener("click", () => {
     console.log("confirm project name");
+    getProjectName();
     createAddProject();
-  })
+  });
 
   btnCancel.addEventListener("click", () => {
-    console.log("cancel project name");
-  })
+    cancelInputProject();
+  });
 }
 
+// gets/checks user input for new project and calls function to create new
+function getProjectName() {
+  const userInput = document.getElementById("input-project-name").value;
+  const cleanedInput = userInput.trim();
+  if(cleanedInput.length > 0) {
+    const project = projectManager.addProject(userInput);
+    createNewProject(project);
+    
+  } else {
+    console.log("Please enter valid Project Name");
+  }
+}
+
+function createNewProject(project) {
+  const listProjectName = document.getElementById("li-project-name");
+  // remove "li-project-name"
+  if(listProjectName) {
+    listProjectName.remove();
+  }
+  const list = document.createElement("li");
+  const button = document.createElement("button");
+
+  button.id = project.getId();
+  button.textContent = project.getName();
+
+  list.appendChild(button);
+  // append new li at the end of projectList
+  projectList.append(list);
+}
+
+function cancelInputProject() {
+  // remove input
+  const listProjectName = document.getElementById("li-project-name");
+  if (listProjectName) {
+    listProjectName.remove();
+  }
+  // add project button
+  createAddProject();
+}
 
 function initialSetup() {
   console.log("hello");
-  createAddProject()
+  createAddProject();
 }
 
+const projectManager = new ProjectManager();
 initialSetup();
